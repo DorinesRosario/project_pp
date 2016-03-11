da29be779f5f13983f080db631b6c016ee117593
DorinesRosario <dorines.sr@gmail.com>
2016-03-11 23:46:18 +0100

    no space! ?? creating psi-blast svm new format

--- 11.new_psi-blast_svm_format/bash_psi-blast_topology.sh
+++ 11.new_psi-blast_svm_format/bash_psi-blast_topology.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 input_files=/home/rosario/Desktop/project_pp/11.new_psi-blast_svm_format/psi-blast_svm_step1/
 output_files=/home/rosario/Desktop/project_pp/11.new_psi-blast_svm_format/psi-blast_svm_step3/
-
 for file in $input_files/*.svm; do
 	base=`basename $file .svm`
 	python psi-blast_svm_topology.py $input_files/$base.topology.txt $input_files/$base.svm $output_files/$base.svm.txt
